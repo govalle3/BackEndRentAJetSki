3f816168248e3cae623b795f0f606a77c4808120
German Ovalle Silva <govalle3@outlook.com>
2021-03-18 13:55:13 -0500

    GradleW instalado, sexta prueba de jenkins

--- microservicio/dominio/src/test/java/com/ceiba/usuario/servicio/ServicioCrearAlquilerTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/usuario/servicio/ServicioCrearAlquilerTest.java
@@ -1,24 +1,17 @@
 package com.ceiba.usuario.servicio;
 
+import com.ceiba.BasePrueba;
+import com.ceiba.dominio.excepcion.ExcepcionDuplicidad;
 import com.ceiba.usuario.modelo.entidad.Alquiler;
 import com.ceiba.usuario.puerto.repositorio.RepositorioAlquiler;
-import com.ceiba.dominio.excepcion.ExcepcionDuplicidad;
 import com.ceiba.usuario.servicio.testdatabuilder.AlquilerTestDataBuilder;
-import mockit.Expectations;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-
-import com.ceiba.BasePrueba;
 
 import java.time.Clock;
 import java.time.Instant;
-import java.time.LocalDateTime;
 import java.time.ZoneId;
 
-
-
-import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class ServicioCrearAlquilerTest {
@@ -50,35 +43,4 @@ public class ServicioCrearAlquilerTest {
         verify(repositorioAlquiler,times(1)).crearAlquiler(alquiler);
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    @Test
-    public void validarDiaMantenimiento() {
-
-        Clock clock = Clock.fixed(Instant.parse("2014-12-23T10:15:30.00Z"), ZoneId.of("UTC"));
-        LocalDateTime dateTimeExpected = LocalDateTime.now(clock);
-        new Expectations(LocalDateTime.class) {
-            {
-                LocalDateTime.now();
-                result = dateTimeExpected;
-            }
-        };
-
-        LocalDateTime now = LocalDateTime.now();
-
-        assertEquals(now.toString(), dateTimeExpected);
-
-    }
 }
--- microservicio/dominio/src/test/java/com/ceiba/usuario/servicio/ServicioPagarAlquilerTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/usuario/servicio/ServicioPagarAlquilerTest.java
@@ -9,63 +9,20 @@ import static org.junit.Assert.*;
 public class ServicioPagarAlquilerTest {
 
     @Test
-    public void pagarAlquilerJetSkiPrecio1() {
+    public void pagarAlquilerJetSkiPrecio1SinMulta() {
         // Arrange
         Alquiler alquiler = new AlquilerTestDataBuilder().conIdJetSki("BC01").conRentTime(10).conDateAndTimeRent(LocalDateTime.of(2021,
-                Month.MARCH, 17, 14, 24, 00)).build();
+                Month.MARCH, 18, 13, 13, 00)).build();
         ServicioPagarAlquiler servicioPagarAlquiler = new ServicioPagarAlquiler();
         // Act
         double result = servicioPagarAlquiler.pagarAlquiler(alquiler);
         // Assert
-        assertEquals(result,78000,0);
+        assertEquals(result,446000,0);
     }
-    @Test
-    public void pagarAlquilerJetSkiPrecio2() {
-        // Arrange
-        Alquiler alquiler = new AlquilerTestDataBuilder().conIdJetSki("BC02").conRentTime(10).conDateAndTimeRent(LocalDateTime.of(2015,
-                Month.JULY, 29, 19, 30, 40)).build();
-        ServicioPagarAlquiler servicioPagarAlquiler = new ServicioPagarAlquiler();
-        // Act
-        double result = servicioPagarAlquiler.pagarAlquiler(alquiler);
-        // Assert
-        assertEquals(result,70000,0);
-    }
-    @Test
-    public void pagarAlquilerJetSkiPrecio3() {
-        // Arrange
-        Alquiler alquiler = new AlquilerTestDataBuilder().conIdJetSki("BC03").conRentTime(10).conDateAndTimeRent(LocalDateTime.of(2015,
-                Month.JULY, 29, 19, 30, 40)).build();
-        ServicioPagarAlquiler servicioPagarAlquiler = new ServicioPagarAlquiler();
-        // Act
-        double result = servicioPagarAlquiler.pagarAlquiler(alquiler);
-        // Assert
-        assertEquals(result,90000,0);
-    }
+
 
     @Test
     public void pagarAlquilerJetSkiPrecio1ConMulta() {
-        // Arrange
-        Alquiler alquiler = new AlquilerTestDataBuilder().conIdJetSki("BC01").conRentTime(10).conDateAndTimeRent(LocalDateTime.of(2021,
-                Month.MARCH, 17, 8, 05, 00)).build();
-        ServicioPagarAlquiler servicioPagarAlquiler = new ServicioPagarAlquiler();
-        // Act
-        double result = servicioPagarAlquiler.pagarAlquiler(alquiler);
-        // Assert
-        assertEquals(result,78000,0);
-    }
-    @Test
-    public void pagarAlquilerJetSkiPrecio2ConMulta() {
-        // Arrange
-        Alquiler alquiler = new AlquilerTestDataBuilder().conIdJetSki("BC02").conRentTime(10).conDateAndTimeRent(LocalDateTime.of(2015,
-                Month.JULY, 29, 19, 30, 40)).build();
-        ServicioPagarAlquiler servicioPagarAlquiler = new ServicioPagarAlquiler();
-        // Act
-        double result = servicioPagarAlquiler.pagarAlquiler(alquiler);
-        // Assert
-        assertEquals(result,70000,0);
-    }
-    @Test
-    public void pagarAlquilerJetSkiPrecio3ConMulta() {
         // Arrange
         Alquiler alquiler = new AlquilerTestDataBuilder().conIdJetSki("BC03").conRentTime(10).conDateAndTimeRent(LocalDateTime.of(2015,
                 Month.JULY, 29, 19, 30, 40)).build();
