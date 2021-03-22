62f934a1fe18fe17cef411074aa1cfd42903f824
German Ovalle Silva <govalle3@outlook.com>
2021-03-22 09:46:32 -0500

    Se define la logica de negocio completa, se realiza una prueba en postman con fallo en el post de pagos. Se comienzan las correciones y se procede con los test

--- microservicio/dominio/src/main/java/com/ceiba/usuario/servicio/ServicioPagarAlquiler.java
+++ microservicio/dominio/src/main/java/com/ceiba/usuario/servicio/ServicioPagarAlquiler.java
@@ -13,10 +13,13 @@ public class ServicioPagarAlquiler {
     double multaMinuto = 0.8;
 
     public ServicioPagarAlquiler(RepositorioAlquiler repositorioAlquiler) {
+
         this.repositorioAlquiler = repositorioAlquiler;
+
     }
 
     public double pagarAlquiler(Alquiler alquiler) {
+
         double valorMinuto = validarValorMinuto(alquiler);
         double totalMulta = calcularSiHayMultaYPagoParcial(alquiler, valorMinuto);
         double totalAPagar = sumaPagoParcialYPagoTotal(alquiler, valorMinuto, totalMulta);
@@ -25,22 +28,32 @@ public class ServicioPagarAlquiler {
 
     }
 
-
     private double validarValorMinuto(Alquiler alquiler) {
 
         String idJetSki = alquiler.getIdJetSki();
+
         double valorMinuto = 0;
 
         if (idJetSki.toString() == "BC01") {
+
             valorMinuto = 5000;
+
         }
+
         if (idJetSki.toString() == "BC02") {
+
             valorMinuto = 7000;
+
         }
+
         if (idJetSki.toString() == "BC03") {
+
             valorMinuto = 9000;
+
         }
+
         return valorMinuto;
+
     }
 
     private double calcularSiHayMultaYPagoParcial(Alquiler alquiler, double valorMinuto) { // separar metodos en 2
@@ -48,31 +61,45 @@ public class ServicioPagarAlquiler {
         double totalMulta = 0;
 
         Integer tiempoRentado = alquiler.getRentTime();
+
         LocalTime fechaYHoraRentaUsuario = alquiler.getDateAndTimeRent().toLocalTime();
+
         LocalTime localTime = LocalTime.now();
+
         Integer duracion = Long.valueOf(Duration.between(fechaYHoraRentaUsuario, localTime).toMinutes()).intValue();
 
         if (duracion > tiempoRentado) {
 
             int minutosPasados = duracion - tiempoRentado;
+
             totalMulta = minutosPasados * multaMinuto * valorMinuto;
+
         }
 
         return totalMulta;
+
     }
 
     private double sumaPagoParcialYPagoTotal(Alquiler alquiler, double valorMinuto, double totalMulta){
 
         double totalAPagar = 0;
+
         double totalParcial = 0;
+
         Integer tiempoRentado = alquiler.getRentTime();
+
         totalParcial = valorMinuto * tiempoRentado;
+
         totalAPagar = totalParcial + totalMulta;
+
         return totalAPagar;
+
     }
 
     private boolean ejecutarPago(Alquiler alquiler) {
+
         return repositorioAlquiler.crearPago(alquiler);
+
     }
 
 }
