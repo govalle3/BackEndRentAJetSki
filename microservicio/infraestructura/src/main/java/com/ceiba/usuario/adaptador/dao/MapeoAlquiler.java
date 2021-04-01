78b5ffeaddfc980f9903d344ba944ddba91ecbca
govalle3 <govalle3@outlook.com>
2021-03-31 22:50:38 -0500

    Datos A Jenkins, bajando deuda tecnica 2

--- microservicio/infraestructura/src/main/java/com/ceiba/usuario/adaptador/dao/MapeoAlquiler.java
+++ microservicio/infraestructura/src/main/java/com/ceiba/usuario/adaptador/dao/MapeoAlquiler.java
@@ -6,7 +6,6 @@ import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class MapeoAlquiler implements RowMapper<DtoAlquiler>, MapperResult {
